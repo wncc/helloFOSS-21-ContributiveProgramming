76b5eb3e18fe9ae4c246ece94184e99b8bbf5ef7
govindsaju <govind@LAPTOP-L36T3QIR.localdomain>
2021-10-17 16:46:15 +0530

    added KMP file

---
+++ Advanced/KMP Algorithm/govindsaju.cpp
@@ -0,0 +1,92 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+vector<int> find_lps(const string &s)
+{
+    int n = s.length();
+    vector<int> res(n);
+    for (int i=1;i<n;i++)
+    {
+        int x = res[i-1];
+        while (x>0 and s[i]!=s[x])
+        {
+            x = res[x-1];
+        }
+        if (s[i]==s[x])
+        {
+            res[i] = x+1;
+        }
+        else
+        {
+            res[i] = x;
+        }
+    }
+    return res;
+}
+
+
+int main()
+{
+    freopen("input.txt","r",stdin);
+    int n;
+    string word;
+    cin>>n>>word;
+    vector<int> lps = find_lps(word);
+    string curr;
+    for (int t=0;t<n;t++)
+    {
+        cin>>curr;
+        int l = curr.length();
+        int i = 0, j = 0;
+        vector<int> occurances;
+        // for (i=0;i<l;i++)
+        // {
+        //     while (j>0 and curr[i]!=word[j])
+        //     {
+        //         j = lps[j-1];
+        //     }
+        //     if (curr[i]==word[j]) j++;
+        //     if (j==word.length())
+        //     {
+        //         occurances.push_back(i-word.length()+1);
+        //         j = lps[n-1];
+        //         i--;
+        //     }
+        // }
+        while (i<l)
+        {
+            bool foundhere = false;
+            if (curr[i]==word[j])
+            {
+                i++;
+                j++;
+                if (j==word.length())
+                {
+                    occurances.push_back(i-j);
+                    foundhere = true;
+                }
+            }
+            if (foundhere==false)
+            {
+                if (i<l and curr[i]!=word[j])
+                {
+                    if (j>0)
+                    {
+                        j = lps[j-1];
+                    }
+                    else i++;
+                }
+            }
+
+        }
+
+        cout<<occurances.size()<<endl;
+        for (auto u:occurances)
+        {
+            cout<<u<<" ";
+        }
+        cout<<endl;
+
+
+    }
+}
